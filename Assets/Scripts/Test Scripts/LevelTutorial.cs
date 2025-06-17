0921486928c7f29cf1bfdbc57ceae8fffdbaa815
jesucristo <juan.bustamantea@estudiantes.arcos.cl>
2025-06-17 17:43:40 -0400

    Testiculoide (Dia de Presentacion)

--- Assets/Scripts/Test Scripts/LevelTutorial.cs
+++ Assets/Scripts/Test Scripts/LevelTutorial.cs
@@ -6,16 +6,24 @@ public class LevelTutorial : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] GameObject movementPrompt;
+
     private AllInputManager allInputManager;
+    private bool wasTutorialFinised;
 
     private void Start()
     {
         allInputManager = AllInputManager.Instance;
+        wasTutorialFinised = false;
     }
     private void Update()
     {
-        Movement();
-        SwitchView();
+        if (wasTutorialFinised == false)
+        {
+            Movement();
+            SwitchView();
+            Shooting();
+        }
+
     }
     private void Movement()
     {
@@ -23,20 +31,27 @@ public class LevelTutorial : MonoBehaviour
         if (movement != Vector2.zero)
         {
             animator.Play("Move Fade Out");
-            SwitchView();
         }
     }
     private void SwitchView()
     {
         float switchView = allInputManager.IsometricSwitch();
-        float shoot = allInputManager.Shoot();
+
         if (switchView == 1)
         {
             animator.Play("Shoot Fade In");
         }
-        if (shoot == 1)
+    }
+    private void Shooting()
+    {
+        float shoot = allInputManager.Shoot();
+        if (shoot ==1)
         {
-            animator.Play("Shoot Fade In");
+            animator.Play("Reload");
         }
+    }
+    public void TutorialFinished()
+    {
+        wasTutorialFinised = true;
     }
 }
